99ddf8d223784ef85c5f7e2e4a7ceb24f559ccc1
ARAB <ahmedmohamed23345@gmail.com>
2019-09-16 12:21:37 +0200

    ch

--- app/src/main/java/com/creative/share/apps/wash_squad_driver/activities_fragments/activity_home/activity/HomeActivity.java
+++ app/src/main/java/com/creative/share/apps/wash_squad_driver/activities_fragments/activity_home/activity/HomeActivity.java
@@ -185,7 +185,7 @@ binding.tvLogout.setOnClickListener(new View.OnClickListener() {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1002 && resultCode == RESULT_OK && data != null) {
+        if (requestCode == 1002 ) {
             Intent intent = getIntent();
            startActivity(intent);
             finish();
